1f0772d625818e7982ab251eac55995d572470c9
alessandro98nascimento <alessandro-s-nascimento@hotmail.com>
2023-02-27 16:42:48 -0300

    para-fixar-04 parte 3

--- fundamentos/secao-03-introducao-a-JavaScript/dia-01-JavaScript-primeiros-passos/paraFixar-04.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-JavaScript-primeiros-passos/paraFixar-04.js
@@ -27,3 +27,22 @@ if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "qu
 else if (weekDay === "sábado" || weekDay === "domingo"){
     console.log("FINALMENTE, descanso merecido! UwU!");
 }
+
+let statusProcessoCeletivo = "lista";
+
+switch (statusProcessoCeletivo) {
+    case "aprovada":
+        console.log("Parabéns, você está no grupo de pessoas aprovadas!");
+        break;
+
+    case "lista":
+        console.log("Você está na nossa lista de espera.")
+        break;
+    
+    case "reprovada":
+        console.log("Infelizmente, você reprovou.")
+        break;
+    
+    default:
+        console.log("Informação incorreta.");
+}
