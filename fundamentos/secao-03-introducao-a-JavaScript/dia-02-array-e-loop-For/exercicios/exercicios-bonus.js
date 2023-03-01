4f69a2c13c73c40fb7b56686d96e69a1bed7e7ec
alessandro98nascimento <alessandro-s-nascimento@hotmail.com>
2023-02-28 22:51:57 -0300

    nao fiz esse conteudo adc...

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-02-array-e-loop-For/exercicios/exercicios-bonus.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+for(let index = 0; index < numbers.length; index += 1){
+    for(let indexNumber = 0; indexNumber < numbers.length; indexNumber += 1){
+        if(numbers[index] < numbers[indexNumber]){
+            let posicao = numbers[indexNumber];
+            numbers[posicao] = 
+        }
+    }     
+}
+console.log(menorParaMaior);
