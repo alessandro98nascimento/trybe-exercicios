107209f0e9df4546999d2bbbc329197bae78c854
alessandro98nascimento <alessandro-s-nascimento@hotmail.com>
2023-03-02 22:11:36 -0300

    exercicio-02

--- fundamentos/secao-03-introducao-a-JavaScript/dia-04-js-objeto/exercicios/exercicio-02.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-04-js-objeto/exercicios/exercicio-02.js
@@ -9,4 +9,14 @@ let leitor = {
         editora: 'Tordesilhas',
       },
     ],
-  };
+};
+
+
+//console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + leitor.livrosFavoritos[0].titulo);
+
+leitor.livrosFavoritos.push({titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+autor: 'JK Rowling',
+editora: 'Rocco',});
+
+console.log(leitor);
+console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos");
