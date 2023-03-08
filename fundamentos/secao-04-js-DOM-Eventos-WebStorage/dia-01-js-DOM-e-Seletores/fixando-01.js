b44c1a6f225c1a5110800fe514197f607e7187ff
alessandro98nascimento <alessandro-s-nascimento@hotmail.com>
2023-03-08 16:21:42 -0300

    finalizando modulo 3 e inicio do 4

---
+++ fundamentos/secao-04-js-DOM-Eventos-WebStorage/dia-01-js-DOM-e-Seletores/fixando-01.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Harry Potter";
+document.getElementById("second-paragraph").innerText = "O melhor da série de filmes";
+document.getElementById("subtitle").innerText = "Mais amado pelos fãns";
