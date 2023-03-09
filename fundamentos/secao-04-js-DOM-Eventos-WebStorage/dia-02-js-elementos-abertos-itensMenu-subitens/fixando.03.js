55776323bea7aa40b0bdafc802bb0ff67c0233f0
alessandro98nascimento <alessandro-s-nascimento@hotmail.com>
2023-03-09 18:29:17 -0300

    finalizando fixandos

---
+++ fundamentos/secao-04-js-DOM-Eventos-WebStorage/dia-02-js-elementos-abertos-itensMenu-subitens/fixando.03.js
@@ -0,0 +1,6 @@
+let elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+
+let capturaH2 = elementoOndeVoceEsta.firstElementChild;
+
+
+elementoOndeVoceEsta.removeChild(capturaH2);
