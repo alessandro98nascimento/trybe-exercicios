2b4b4b8d28b78b111dad406fd2a915e2b32caa07
alessandro98nascimento <alessandro-s-nascimento@hotmail.com>
2023-03-13 20:24:30 -0300

    finalizando js

--- fundamentos/secao-04-js-DOM-Eventos-WebStorage/dia-04-Web-storage/exercicios.js
+++ fundamentos/secao-04-js-DOM-Eventos-WebStorage/dia-04-Web-storage/exercicios.js
@@ -1,7 +1,16 @@
 const texto = document.querySelector(".description");
+
 const corFundo = document.getElementById("background-color").children;
+
 const corTexto = document.getElementById("font-color").children;
+
 const tamanhoFonte = document.getElementById("font-size").children;
+
+const espaçoLinhas = document.getElementById("line-height").children;
+
+const estiloDeFonte = document.getElementById("font-family").children;
+
+
 
 const mudaFundo = () => {
     for(let index = 1; index < corFundo.length; index += 1){
@@ -66,7 +75,47 @@ const mudaTamanhoFonte = () => {
     }
 }
 
+const mudaEspaçoLinhas = () => {
+    for(let index = 1; index < espaçoLinhas.length; index += 1){
+        let botao = espaçoLinhas[index];
+        botao.addEventListener("click", () => {
+            if(botao.innerText === "1"){
+                texto.style.lineHeight = "1";
+            }
+            if(botao.innerText === "normal"){
+                texto.style.lineHeight = "normal";
+            }
+            if(botao.innerText === "1.5"){
+                texto.style.lineHeight = "1.5";
+            }
+            if(botao.innerText === "2.0"){
+                texto.style.lineHeight = "2.0";
+            }
+            if(botao.innerText === "3.0"){
+                texto.style.lineHeight = "3.0";
+            }
+        })
+    }
+}
+
+const mudaEstiloFonte = () => {
+    for(let index = 1; index < estiloDeFonte.length; index += 1){
+        let botao = estiloDeFonte[index];
+        botao.addEventListener("click", () => {
+            if(botao.innerText === "Arial"){
+                texto.style.fontFamily = "Arial";
+            }
+            if(botao.innerText === "Times New Roman"){
+                texto.style.fontFamily = "Times, New Roman"
+            }
+        })
+    }
+}
+
+
 
 mudaFundo();
 mudaCorTexto();
 mudaTamanhoFonte();
+mudaEspaçoLinhas();
+mudaEstiloFonte();
